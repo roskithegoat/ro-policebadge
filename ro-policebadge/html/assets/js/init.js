a467cf9c15603d6fe110461eeffb4b6864339cca
Roski <73050572+Roski-cpu@users.noreply.github.com>
2023-02-13 02:20:19 +0545

    Added Main Files!

---
+++ ro-policebadge/html/assets/js/init.js
@@ -0,0 +1,35 @@
+const open = (data) => {
+  $('#name').css('color', '#282828');
+
+  $('#name').text(data.name);
+  $('#dob').text(data.dateofbirth);
+  $('#rank').text(data.rank);
+  $('#csi').text(data.csi);
+
+  $('#id-card').css('background', 'url(assets/images/lspd.png)');
+
+  $('#id-card').show();
+}
+
+const close = () => {
+  $('#name').text('');
+  $('#dob').text('');
+  $('#height').text('');
+  $('#signature').text('');
+  $('#sex').text('');
+  $('#id-card').hide();
+  $('#licenses').html('');
+}
+
+$(document).ready(function(){
+    window.addEventListener('message', function(event) {
+        switch(event.data.action) {
+            case "open":
+                open(event.data);
+                break;
+            case "close":
+                close();
+                break;
+        }
+    })
+});
