ce6864bff9cb4c401ab0c8501bf5263322b43d65
Roski <73050572+Roski-cpu@users.noreply.github.com>
2023-02-13 02:30:31 +0545

    Add files via upload

---
+++ sv_badge.lua
@@ -0,0 +1,20 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterServerEvent('badge:open')
+AddEventHandler('badge:open', function(ID, targetID, type)
+	local Player = QBCore.Functions.GetPlayer(ID)
+
+	local data = {
+		name = Player.PlayerData.charinfo.firstname.." "..Player.PlayerData.charinfo.lastname,
+		rank = Player.PlayerData.job.grade.name,
+		csi = Player.PlayerData.metadata.callsign
+		
+	}
+
+	TriggerClientEvent('badge:open', targetID, data)
+	TriggerClientEvent( 'badge:shot', targetID, source )
+end)
+
+QBCore.Functions.CreateUseableItem('specialbadge', function(source, item)
+    TriggerClientEvent('badge:openPD', source, true)
+end)
